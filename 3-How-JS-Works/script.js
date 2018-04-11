df1bd8c32c382442d0bfb26bcd9f588b0648ae8b
Ruth Laird <ruth.laird@anaplan.com>
2018-04-11 19:23:28 +0100

    Lecture 3 - How JS Works

---
+++ 3-How-JS-Works/script.js
@@ -0,0 +1,118 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+
+/*
+//Functions
+calculateAge(1965);
+
+function calculateAge(year){
+    console.log(2016 - year);
+}
+
+//retirement(1990); //Uncaught TypeError: retirement is not a function
+var retirement = function(year){
+    console.log(65 - (2016 - year));
+}
+
+retirement(1990); //51
+
+//Variables
+console.log(age); //undefined
+var age = 23;
+console.log(age); //23
+
+function foo(){
+    console.log(age); //undefined
+    var age = 65;
+    console.log(age); //65
+}
+foo();
+console.log(age); //23
+*/
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+/*
+// First scoping example
+
+var a = 'Hello! ';
+first();
+
+function first() {
+    var b = 'Hi! ';
+    second();
+
+    function second() {
+        var c = 'Hey! ';
+        console.log(a + b + c);
+    }
+}
+
+
+// Example to show the differece between execution stack and scope chain
+
+
+var a = 'Hello! ';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third();
+    }
+}
+
+function third() {
+    var d = 'John';
+//    console.log(a + b + c + d); //Uncaught ReferenceError: b is not defined
+//    console.log(c);   //Uncaught ReferenceError: c is not defined
+    console.log(a+d); //Hello! John
+}
+
+*/
+///////////////////////////////////////
+// Lecture: The this keyword
+
+//console.log(this);
+
+/*
+calculateAge(1985);
+
+function calculateAge(year){
+    console.log(2016 - year);
+    console.log(this);
+}
+
+*/
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
+        
+        function innerFunction(){
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+}
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+
+
+
+
