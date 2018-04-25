80362840a0d0b7d94ef84ff7d3a4261430baa440
Ruth Laird <ruth.laird@anaplan.com>
2018-04-25 21:09:38 +0100

    Lecture 6 - Budget app WIP

---
+++ 6-Budgety/app.js
@@ -0,0 +1,131 @@
+// BUDGET CONTROLLER
+var budgetContoller = (function() {
+    
+    //Function constructors
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+    
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            
+            //Create new ID (last ID + 1)
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+                        
+            //Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            
+            //Push it in to our data structure
+            data.allItems[type].push(newItem);
+            
+            //Return the new element
+            return newItem;
+        },
+        
+        testing: function() {
+            console.log(data);
+        }
+    }
+    
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+  
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDesciption: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
+    
+    return {
+      getinput: function() {
+          return {
+            type: document.querySelector(DOMstrings.inputType).value,    //will be either 'inc' or 'exp'
+            description: document.querySelector(DOMstrings.inputDesciption).value,
+            value: document.querySelector(DOMstrings.inputValue).value,
+          };
+      },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+    
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var setupEventListeners = function() {
+        
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    
+        document.addEventListener('keypress', function(event) {
+            //can use 'keycode' for newer browsers, need to use 'which' for older browsers
+            if (event.keyCode === 13 || event.which == 13) {
+                ctrlAddItem();
+            };
+        });
+    }; 
+     
+    var ctrlAddItem = function() {
+        
+        var input, newItem;
+        
+        // 1. Get the field input data
+        input = UICtrl.getinput();
+        console.log(input);
+        
+        // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        
+        // 3. Add the item to the UI
+        
+        // 4. Calculate the budget
+        
+        // 5. Display the budget on the UI
+    };
+    
+
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    };
+    
+})(budgetContoller, UIController);
+
+controller.init();
