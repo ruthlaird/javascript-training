3b289b6f882b192a4aa719ae6405d66e09d3c871
Ruth Laird <ruth.laird@anaplan.com>
2018-06-02 21:31:28 +0100

    9-Forkify: How ES6 modules work

--- 9-Forkify/src/js/index.js
+++ 9-Forkify/src/js/index.js
@@ -1,5 +1,11 @@
 // Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+//Default imports
+import str from './models/Search';
 
+//Named imports - can either do this:
+import { add, multiply as m, ID } from './views/searchView';
+console.log(`Using named imported functions (method 1)! add: ${add(ID, 2)} and multiply: ${m(3, 5)}. str: ${str}`);
+
+//or another way of doing named imports
+import * as searchView from './views/searchView';
+console.log(`Using named imported functions (method 2)! add: ${searchView.add(searchView.ID, 2)} and multiply: ${searchView.multiply(3, 5)}. str: ${str}`);
